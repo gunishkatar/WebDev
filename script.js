f5a21f1e40b8576beaae52dbb053db8d2723e8ed
gunishkatar <23401710-gunishkatar@users.noreply.replit.com>
2023-05-27 19:28:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//-----------------------------CHAPTER 1-----------------------------
+console.log('Hello, World')
+console.log('Gunish Katar')
+
+//Variables
+name = 'Peter Pan'
+console.log(name)
+sentence = 'How are you today'
+console.log(sentence)
+
+//Operators
+// Basic Operators, * / ** + -
+food = Number(prompt('What is the food amount'))
+tip = Number(prompt('Enter the tip percentage'))/100
+tipAmount = food*tip
+
+// CONVERTINT STRING 100 INTO NUMBER 100
+totalFoodCost = food + tipAmount
+
+console.log('Tip amount', tipAmount)
+console.log('Total', totalFoodCost)
+
+//NOTE: JAVASCRIPT VARIABLES CVAN HOLD DIFFERENT DATA TYPES: NUMBERS, STRINGS, OBJECTS, AND MORE.
+// IN THE CASE MENTIONED ABOVE, WE HANDLED NUMBER AS TEXT WHICH CAUSED THIS WEIRD OUTCOME.
+//NOTE: TO CONVERT STRING 100 INTO NUMBER 100 WE HAVE TO USE THE FUNCTION CALLED NUMBER. WE HAVE TO WRAP THE STRING VALUE OF THE NUMBER
+
+//User Input
+// the prompt() is a built in javaScript functionality that helps you get inputs from a user through the browser.
+
+//NOTE: IN RELAITY WE DONT USE PROMPTS OR ALERTS. WE RATHER USE FORMS, INPUTS, OR MORE COMPLICATED STUFF.
+
+//XXXXXXXXXXXXXXXXXXXXXXXX------------------------XXXXXXXXXXXXXXXXXXXXXXXXX
+
+//-----------------------------CHAPTER 2-----------------------------
+
+// DATA TYPES
+//Some of the data types are
+// 1. Number: integer and decimals
+// 2. String: '' or ""
+// 3. Arrays: []
+// 4. Objects: {}
+// 5. booleans: true or false
+
+// BUILD A WEATHER APP
+
+//XXXXXXXXXXXXXXXXXXXXXXXX------------------------XXXXXXXXXXXXXXXXXXXXXXXXX
